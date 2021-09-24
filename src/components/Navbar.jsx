4bf1ff38e33bf0096922d7b1aa62690f336ff970
mayur249 <kundermayuresh249@gmail.com>
2021-09-24 11:25:02 +0530

    added and and other componenets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,33 @@ import {
   FundOutlined,
   MenuOutlined,
 } from "@ant-design/icons";
+import icon from "../images/cryptocurrency.png";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large"/>
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Crypton</Link>
+        </Typography.Title>
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined/>}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined/>}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined/>}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined/>}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      </div>
+    </div>
+  ) ;
 };
 
 export default Navbar;
