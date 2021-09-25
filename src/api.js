4d512e350d3a567f0e84fd9e5000126a55028c48
mayur249 <kundermayuresh249@gmail.com>
2021-09-25 22:01:43 +0530

    exporting api keys

---
+++ src/api.js
@@ -0,0 +1,5 @@
+export const CRYPTO_HOST = process.env.REACT_APP_CRYPTO_HOST;
+export const CRYPTO_KEY = process.env.REACT_APP_CRYPTO_KEY;
+export const CRYPTO_NEWS_HOST = process.env.REACT_APP_CRYPTO_NEWS_HOST;
+export const CRYPTO_NEWS_KEY = process.env.REACT_APP_CRYPTO_NEWS_KEY;
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
