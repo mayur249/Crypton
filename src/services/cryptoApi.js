d0eaec9b38608e71c429e9eb319f59110315644b
mayur249 <kundermayuresh249@gmail.com>
2021-09-25 15:49:07 +0530

    settted up redux/toolkit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "bb598bab56msh51ca62eb2dd2c0fp125c36jsn967970df3a19",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': 'bb598bab56msh51ca62eb2dd2c0fp125c36jsn967970df3a19'
+//     }
+//   };
